65653b13cc81629c30a6cf276e999a554633a0e5
ThanhTan <thanhtan27032001@gmail.com>
2024-01-07 16:11:00 +0700

    [v1.0.0] General - first commit

---
+++ lib/infinite_list.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class InfiniteList extends StatelessWidget {
+  const InfiniteList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView.builder(
+          itemBuilder: (context, index) {
+            return const ListTile(
+              leading: Text('1'),
+              title: Text('This is title', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+              subtitle: Text('This is subtitle'),
+              isThreeLine: true,
+            );
+          },
+          itemCount: 5,
+        ),
+      ),
+    );
+  }
+}
